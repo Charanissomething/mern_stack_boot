07743f2e9e14e6a926494f8899c51cb64763f4e4
Charanissomething <ramasaicharangudapureddy@gmail.com>
2023-02-13 22:24:11 +0530

    new changes..

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = exp();
 const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res) {
-    res.send("iam cherry!!!");
+    // res.send("iam cherry!!!");
+    res.sendFile(__dirname + "/index.html");
 });
 app.listen(port, function() {
     console.log("server running on http://localhost:" + port);
