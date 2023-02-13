0efb8912247edcbc00c9b37ff39670c9cd672524
Charanissomething <ramasaicharangudapureddy@gmail.com>
2023-02-13 23:38:17 +0530

    commits

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ app.get("/", function(req, res) {
     // res.send("iam cherry!!!");
     res.sendFile(__dirname + "/index.html");
 });
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/resume.html");
-});
+
 app.listen(port, function() {
     console.log("server running on http://localhost:" + port);
 });
