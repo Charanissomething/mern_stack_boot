bf712f7a9028fdd5571675f690615a03d1ff6f03
Charanissomething <ramasaicharangudapureddy@gmail.com>
2023-02-15 00:08:29 +0530

    commit

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const exp = require('express');
 const app = exp();
 const port = process.env.PORT || 5010;
 
+const options = {
+    extensions: ['htm', 'html', 'css', 'js', 'ico', 'jpg', 'jpeg', 'png', 'svg', 'pdf'],
+    index: ['index.html'],
+}
+app.use(express.static("public", options));
+
 app.get("/", function(req, res) {
     // res.send("iam cherry!!!");
     res.sendFile(__dirname + "/index.html");
@@ -9,6 +15,10 @@ app.get("/", function(req, res) {
 app.get("/resume", function(req, res) {
     // res.send("iam cherry!!!");
     res.sendFile(__dirname + "/resume.html");
+});
+app.get("/resume", function(req, res) {
+    // res.send("iam cherry!!!");
+    res.sendFile(__dirname + "/card.html");
 });
 app.listen(port, function() {
     console.log("server running on http://localhost:" + port);
