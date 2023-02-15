e3d8eff5ceffe7b84040342b3d7fee6295eec823
Charanissomething <ramasaicharangudapureddy@gmail.com>
2023-02-15 19:08:14 +0530

    commit

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const exp = require('express');
 const app = exp();
 const port = process.env.PORT || 5010;
 
+const options = {
+    extensions: ['htm', 'html', 'css', 'js', 'ico', 'jpg', 'jpeg', 'png', 'svg', 'pdf'],
+    index: ['index.html'],
+}
+app.use(exp.static("public", options));
+
 app.get("/", function(req, res) {
     // res.send("iam cherry!!!");
     res.sendFile(__dirname + "/index.html");
