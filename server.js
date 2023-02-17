eae836d28f41fe880c207d37d33e8398d11b4ef9
Charanissomething <ramasaicharangudapureddy@gmail.com>
2023-02-17 21:43:57 +0530

    commit

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const exp = require('express');
 const mongoose = require("mongoose");
 
 const app = exp();
+
+app.use(express.static("public", options));
+const options = {
+    extensions: ['htm', 'html', 'css', 'js', 'ico', 'jpg', 'jpeg', 'png', 'svg'],
+    index: ['index.html'],
+}
 const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res) {
@@ -17,6 +23,10 @@ app.get("/resume", function(req, res) {
 app.get("/card", function(req, res) {
     // res.send("iam cherry!!!");
     res.sendFile(__dirname + "/card.html");
+});
+app.get("/weather", function(req, res) {
+    // res.send("iam cherry!!!");
+    res.sendFile(__dirname + "/weather.html");
 });
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err) {
